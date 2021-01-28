02b49e39151247392ce30ad77d243f7457a7a229
gwcew <git.matvey@yandex.ru>
2021-01-28 19:10:56 +0300

    FIX: little fix

--- src/components/VideoGrid/VideoBlock.js
+++ src/components/VideoGrid/VideoBlock.js
@@ -47,7 +47,7 @@ function VideoBlock({ width = 3, videoData }) {
   const classes = useStyles();
 
   return (
-    <Grid item xs={12} sm={6} md={6} lg={3} xl={3} className={classes.root}>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={3} className={classes.root}>
       <Link to={"/video/" + videoData.id}>
       <img src={videoData.url_preview} alt="preview" className={classes.previewImg} />
         <Grid container className={classes.captionWrap} spacing={1}>
