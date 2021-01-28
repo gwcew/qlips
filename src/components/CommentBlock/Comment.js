b07de7755c8f50c35df3c9f8865b9586a1927966
gwcew <git.matvey@yandex.ru>
2021-01-28 09:46:52 +0300

    ADD: some fixes for commentaryblock

--- src/components/CommentBlock/Comment.js
+++ src/components/CommentBlock/Comment.js
@@ -6,10 +6,14 @@ import {
     ListItemText,
     ListItemIcon,
     Grid,
+    IconButton,
+    ListItemSecondaryAction,
+    Button,
 } from '@material-ui/core';
 import {makeStyles, withStyles} from '@material-ui/styles';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import PropTypes from 'prop-types';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -88,6 +92,49 @@ const styles = (theme) => ({
         color: theme.palette.comment.authorNameColor,
         fontSize: 15,
     },
+    buttonOptions: {
+        "&.MuiButtonBase-root": {
+            position: 'absolute',
+            right: 0,
+            bottom: 20,
+          },
+          "&:hover": {
+            borderRadius: '0px',
+            borderBottom: '0px',
+            borderBottomLeftRadius: '0px',
+            borderBottomRightRadius: '0px',
+            backgroundColor: 'transparent',
+          },
+    },
+    buttonOptionsNested: {
+        "&.MuiButtonBase-root": {
+            position: 'absolute',
+            right: 0,
+            bottom: 20,
+          },
+          "&:hover": {
+            borderRadius: '0px',
+            borderBottom: '0px',
+            borderBottomLeftRadius: '0px',
+            borderBottomRightRadius: '0px',
+            backgroundColor: 'transparent',
+          },
+    },
+    buttonAnswer: {
+        "&.MuiButtonBase-root": {
+            position: 'relative',
+            top: 15,
+            left: 20,
+            fontSize: 10,
+          },
+          "&:hover": {
+            borderRadius: '0px',
+            borderBottom: '0px',
+            borderBottomLeftRadius: '0px',
+            borderBottomRightRadius: '0px',
+            backgroundColor: 'transparent',
+          },
+    },
     contentName: {
         color: theme.palette.comment.contentNameColor,
         fontSize: 14,
@@ -95,13 +142,13 @@ const styles = (theme) => ({
 });
 
 function CustomListItemComponent(props) {
-    return <ListItem button className={props.isNested ? props.classes.rootnested : props.classes.root}>
+    return <ListItem button className={props.isNested ? props.classes.rootnested : props.classes.root} disableRipple={true}>
         <ListItemIcon style={{paddingLeft: '10px'}}>
             <AccountCircleIcon style={{width: '48px', height: '48px'}}></AccountCircleIcon>
         </ListItemIcon>
         <ListItemText>
             <Grid container direction="column">
-                <Grid item className={props.classes.authorName} direction="column">
+                <Grid item className={props.classes.authorName}>
                     <div>
                         {props.authorName}
                     </div>
@@ -113,6 +160,12 @@ function CustomListItemComponent(props) {
                 </Grid>
             </Grid>
         </ListItemText>
+        <IconButton className={props.isNested ? props.classes.buttonOptionsNested : props.classes.buttonOptions}>
+                        <MoreHorizIcon className={props.classes.buttonShare}></MoreHorizIcon>
+                    </IconButton>
+                    <Button className={props.classes.buttonAnswer}>
+                        Hello
+                    </Button>
     </ListItem>
 }
 
@@ -131,7 +184,8 @@ function Comment({authorName, contentName, iconURL, replies}) {
     return <>
         <List component="nav">
             <CustomListItem authorName={authorName} contentName={contentName}></CustomListItem>
-            <CustomListItem isNested={true} authorName={"1"} contentName={"2"}></CustomListItem>
+            <CustomListItem isNested={true} authorName={"Сергей Добросмыслов"} contentName={"Здравствуйте! Мы работаем с 8 до 21. Приходите к нам!"}></CustomListItem>
+            <CustomListItem isNested={true} authorName={"Николай Захарченко"} contentName={"Где у вас можно просмотреть ..."}></CustomListItem>
         </List>
         </>
 }
