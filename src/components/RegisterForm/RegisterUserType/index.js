371a9ccb4cb9a3ebbb51d56322b9da229d7853e0
gwcew <git.matvey@yandex.ru>
2021-02-04 14:33:18 +0300

    ADD: added access types, now needed styles

--- src/components/RegisterForm/RegisterUserType/index.js
+++ src/components/RegisterForm/RegisterUserType/index.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import {Grid} from '@material-ui/core';
+import React, {useState} from 'react';
+import {
+    Grid,
+    ToggleButton,
+    ToggleButtonGroup,
+} from '@material-ui/core';
 import PropTypes from 'prop-types';
 import {makeStyles} from '@material-ui/styles';
 import {connect} from 'react-redux';
@@ -8,7 +12,7 @@ import {
     setModalRegisterFormStatus,
     setModalRegisterFormListUserStatus,
 } from 'redux/actions';
-
+import {ACCESS_TYPE_USER_REGISTER_AS_STUDY, ACCESS_TYPE_USER_REGISTER_AS_STUDENT} from './AccessTypes';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -22,10 +26,30 @@ const useStyles = makeStyles(theme => ({
 function Page({handleModalRegisterFormStatus, handleModalRegisterFormUserListStatus}) {
     const classes = useStyles();
 
+    const [alignment, setAlignment] = useState(ACCESS_TYPE_USER_REGISTER_AS_STUDY);
+
+    const handleAlignment = (event, newAlignment) => {
+      setAlignment(newAlignment);
+    };
+
     return (
         <Grid container className={classes.root}>
             <Grid item>
                 <div className={classes.headerText}>Список документов, подтверждающих статус пользователя</div>
+            </Grid>
+            <Grid item>
+                <ToggleButtonGroup
+                value={alignment}
+                exclusive
+                onChange={handleAlignment}
+                >
+                    <ToggleButton value={ACCESS_TYPE_USER_REGISTER_AS_STUDY}>
+                        Обучающийся
+                    </ToggleButton>
+                    <ToggleButton value={ACCESS_TYPE_USER_REGISTER_AS_STUDENT}>
+                        Студент
+                    </ToggleButton>
+                </ToggleButtonGroup>
             </Grid>
         </Grid>
     );
