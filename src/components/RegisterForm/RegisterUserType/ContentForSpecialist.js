db1bfe8c30f872bf9c0af33b66bf0db3f6b706c5
gwcew <git.matvey@yandex.ru>
2021-02-04 15:06:38 +0300

    ADD: content for specialist implemented

--- src/components/RegisterForm/RegisterUserType/ContentForSpecialist.js
+++ src/components/RegisterForm/RegisterUserType/ContentForSpecialist.js
@@ -4,20 +4,58 @@ import {
     ListItem,
     ListItemAvatar,
     ListItemIcon,
+    ListItemText,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import {makeStyles} from '@material-ui/styles';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 
 const useStyles = makeStyles(theme => ({
     root: {
-
+        color: 'white'
+    },
+    icon: {
+        color: 'white',
     },
 }));
 
 function Page({}) {
-    return <List>
+    const classes = useStyles();
 
-    </List>;
+    return <List className={classes.root}>
+        <ListItem>
+            <ListItemIcon>
+                <RadioButtonUncheckedIcon className={classes.icon}></RadioButtonUncheckedIcon>
+            </ListItemIcon>
+            <ListItemText>
+                Документ о высшем профессиональном медицинском образовании и квалификации
+            </ListItemText>
+        </ListItem>
+        <ListItem>
+            <ListItemIcon>
+                <RadioButtonUncheckedIcon className={classes.icon}></RadioButtonUncheckedIcon>
+            </ListItemIcon>
+            <ListItemText>
+                Документ о высшем или среднем профессиональном фармацевтическом образовании и квалификации
+            </ListItemText>
+        </ListItem>
+        <ListItem>
+            <ListItemIcon>
+                <RadioButtonUncheckedIcon className={classes.icon}></RadioButtonUncheckedIcon>
+            </ListItemIcon>
+            <ListItemText>
+                Сертификат специалиста и / или свидетельство об аккредитации специалиста
+            </ListItemText>
+        </ListItem>
+        <ListItem>
+            <ListItemIcon>
+                <RadioButtonUncheckedIcon className={classes.icon}></RadioButtonUncheckedIcon>
+            </ListItemIcon>
+            <ListItemText>
+                Документ, подтверждающий состояние пользователя в трудовых отношениях с медицинской или фармацевтической организацией
+            </ListItemText>
+        </ListItem>
+    </List> 
 }
 
 Page.propTypes = {
